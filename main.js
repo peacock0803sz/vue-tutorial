01ae587cc46c387eab441807a46612a2aae02580
Peacock0803sz <claude0803sz@gmail.com>
2018-12-16 17:09:38 +0900

    fix arrow func

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-var STORAGE_KEY = 'todos-vue.js-demo'
+// https://jp.vuejs.org/v2/examples/todomvc.html
+var STORAGE_KEY = 'todos-vuejs-demo'
 var todoStorage = {
-  fetch: ()=>{
-    var todos = JSON.parse(
-      localStorage.getItem(STORAGE_KEY) || '[]'
-    )
-    todos.forEach((todo, index)=>{
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach(function (todo, index) {
       todo.id = index
     })
     todoStorage.uid = todos.length
     return todos
   },
-  save: todos => {
+  save: function (todos) {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
   }
 }
+
 
 const app = new Vue({
   el: '#app',
@@ -22,7 +22,7 @@ const app = new Vue({
   },
   methods: {
     // todo追加の処理
-    doAdd: (event, value)=>{
+    doAdd: function(event, value) {
       // refで名前をつけておいた要素を参照する
       var comment = this.$refs.comment
       if (!comment.value.length) {
