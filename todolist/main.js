e0d986d57f6d1c860d65c88f0ed98c796d3cff7e
Peacock0803sz <peacock0803sz@gmail.com>
2018-12-23 13:59:57 +0900

    step2

--- todolist/main.js
+++ todolist/main.js
@@ -1,3 +1,16 @@
+const STORAGE_KEY = 'todos-vuejs-demo'
+const todoStorage = {
+  fetch: () => {
+    const todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach((todo, index) => {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: todos => localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+}
+
 const app = new Vue({
     el:app,
     
